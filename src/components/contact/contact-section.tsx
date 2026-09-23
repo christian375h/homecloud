149e7562ffeebaef6f8c7484576cc3f0fae47f71
Christian Hess <christian375h@gmail.com>
2026-09-23 16:11:15 -0600

    fix: contact email

--- src/components/contact/contact-section.tsx
+++ src/components/contact/contact-section.tsx
@@ -42,7 +42,7 @@ export function ContactSection() {
           <div className="text-center">
             <Typography variant="lead" className="mx-auto max-w-2xl text-gray-600">
               This page is currently under construction, feel free to reach out at <br></br>
-              support@homecloud.net
+              support@gethomecloud.net
             </Typography>
           </div>
 
